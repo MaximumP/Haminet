d5a4c070d7d86ba4d7d57c3025628853f9e7711e
Max Schumann <mschumann@de-cix.net>
2024-12-29 22:06:29 +0100

    pull up dht sensor

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from debounce import DebouncedSwitch
 from output import Pager
 from environment_control import EnvironmentControl
 
-dht = DHT22(Pin(22))
+dht = DHT22(Pin(22, Pin.PULL_UP))
 timer = Timer(-1)
 up = Pin(17, Pin.IN, Pin.PULL_DOWN)
 down = Pin(19, Pin.IN, Pin.PULL_DOWN)
@@ -25,7 +25,7 @@ environment_control = EnvironmentControl(
     led_red=Pin(1, Pin.OUT, value=0),
     led_orange=Pin(4, Pin.OUT, value=0),
     led_yellow=Pin(6, Pin.OUT, value=0),
-    config=config
+    config=config,
 )
 pager = Pager(dht, config, environment_control)
 
@@ -47,20 +47,20 @@ def main():
 
 def start_up():
     environment_control._led_orange.value(1)
-    sleep(.5)
+    sleep(0.5)
     environment_control._led_red.value(1)
-    sleep(.5)
+    sleep(0.5)
     environment_control._led_green.value(1)
-    sleep(.5)
+    sleep(0.5)
     environment_control._led_yellow.value(1)
-    sleep(.5)
+    sleep(0.5)
 
     environment_control._led_orange.value(0)
-    sleep(.5)
+    sleep(0.5)
     environment_control._led_red.value(0)
-    sleep(.5)
+    sleep(0.5)
     environment_control._led_green.value(0)
-    sleep(.5)
+    sleep(0.5)
     environment_control._led_yellow.value(0)
 
 
